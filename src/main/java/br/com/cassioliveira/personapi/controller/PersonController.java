5479582b300d0c3ee7e3745e06339823dc51a000
cassioliveira <cassio@cassioliveira.com.br>
2021-08-09 10:32:49 -0300

    save person done

--- src/main/java/br/com/cassioliveira/personapi/controller/PersonController.java
+++ src/main/java/br/com/cassioliveira/personapi/controller/PersonController.java
@@ -1,15 +1,28 @@
 package br.com.cassioliveira.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.cassioliveira.personapi.dto.MessageResponseDTO;
+import br.com.cassioliveira.personapi.entity.Person;
+import br.com.cassioliveira.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/persons")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return  "API Test";
+
+    private PersonRepository personRepository;
+
+    /* Inject the personRespository construtor make easy to implement the unit tests, as author said. Needs more research... */
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    /* The @Requestbody annotation notify this method that a person object comes from client. */
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO.builder().message("Created person with ID" + savedPerson.getId()).build();
     }
 }
---
+++ src/main/java/br/com/cassioliveira/personapi/entity/Person.java
@@ -0,0 +1,42 @@
+package br.com.cassioliveira.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    //@Temporal(TemporalType.DATE)
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+
+}
+
+
+
+
